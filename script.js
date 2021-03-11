ec2a185912e3e91e843666f6a2a093813bd67fd9
Ankit Kumar Singh <ankitchbs333@gmail.com>
2021-03-11 19:03:37 +0530

    slight change in code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var countInterval;
 
 function startCounter() {
@@ -67,64 +66,4 @@ function increaseCount(currentNos, nextNos, index) {
             next.innerText = 0;
         }
     }, 500);
-    
-=======
-var countInterval;
-
-function startCounter() {
-
-    var number = parseInt(document.getElementById("number").value);
-
-    if (isNaN(number)) {
-        alert("Please enter a number");
-        clearInterval(countInterval);   // This is important for the condition when a counter is running and you entered a wrong input for a new counter
-        return;
-    }
-    if (number < 1 || number > 9) {
-        alert("Range out of bounds");
-        clearInterval(countInterval);
-        return;
-    }
-
-    var currentNo = document.querySelector("#counter .current");
-    var nextNo = document.querySelector("#counter .next");
-    var count = 0;
-
-    // If user clicks on 'Start Counter' button again - remove this function and below 2 lines if you don't consider this situation
-    resetNumbers(currentNo, nextNo);
-    
-    // Clears the previous interval that was running
-    clearInterval(countInterval);
-
-    countInterval = setInterval(function () {
-        if (count === number) {
-            clearInterval(countInterval);
-            alert("Counter has stopped");
-            return;
-        }
-        increaseCount(currentNo, nextNo);
-        count++;
-    }, 1000);
-
-}
-
-
-function resetNumbers(currentNo, nextNo) {
-    currentNo.innerText = 0;
-    nextNo.innerText = 1;
-}
-
-
-
-function increaseCount(currentNo, nextNo) {
-
-    nextNo.classList.add("animate");
-
-    setTimeout(function () {
-        currentNo.innerText = nextNo.innerText;
-        nextNo.classList.remove("animate");
-        nextNo.innerText = parseInt(nextNo.innerText) + 1;
-    }, 500);
-
->>>>>>> dbfe37b1275fe38a827fe03de13b0ef5cbd0e2c3
 }
